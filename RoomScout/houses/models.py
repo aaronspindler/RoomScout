b53358c3cdf7aa41a8aaf60a086b7f68096b3f28
Aaron Spindler <nova1104@live.com>
2019-07-15 17:06:06 -0400

    Added utils

--- RoomScout/houses/models.py
+++ RoomScout/houses/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from accounts.models import User
 from django_countries.fields import CountryField
 
+class Tenant(models.Model):
+    pass
+
 class House(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=400)
     country = CountryField()
-    prov_state = models.CharField(max_length = 40)
+    prov_state = models.CharField(max_length = 2)
     postal_code = models.CharField(max_length = 6)
     date_posted = models.DateTimeField(default=None)
     kijiji_link = models.URLField(default=None)

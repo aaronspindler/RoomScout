d9ce68be47f107c61176130decbced21215f282d
Aaron Spindler <nova1104@live.com>
2019-07-15 14:15:18 -0400

    Finished sign up page

--- RoomScout/accounts/views.py
+++ RoomScout/accounts/views.py
@@ -2,22 +2,60 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django_countries import countries
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('home')
-    template_name = 'accounts/signup.html'
+from .models import User
 
-    def form_valid(self, form):
-        view = super(SignUp, self).form_valid(form)
-        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(self.request, user)
-        return view
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username has already been taken!'})
+            except User.DoesNotExist:
+                try:
+                    user = User.objects.get(email = request.POST['email'])
+                    return render(request, 'accounts/signup.html', {'error':'Email is already in use!'})
+                except User.DoesNotExist:
+                    if request.POST['firstname'] and request.POST['lastname']:
+                        email = request.POST['email']
+                        username = request.POST['username']
+                        password = request.POST['password1']
+                        firstname = request.POST['firstname']
+                        lastname = request.POST['lastname']
+                        if request.POST['address']:
+                            address = request.POST['address']
+                        else:
+                            address = None
 
-def additionalinfo(request):
-    return render(request, 'accounts/additionalinfo.html')
+                        if request.POST['province']:
+                            prov_state = request.POST['province']
+                        else:
+                            prov_state = None
+
+                        if request.POST['postal_code']:
+                            postal_code = request.POST['postal_code']
+                        else:
+                            postal_code = None
+
+                        user = User.objects.create_user(username, password=password)
+                        user.email = email
+                        user.firstname = firstname
+                        user.lastname = lastname
+                        user.address = address
+                        user.prov_state = prov_state
+                        user.postal_code = postal_code
+                        user.score = 0.0
+                        user.save()
+                        login(request,user)
+                        return redirect('home')
+                    else:
+                        return render(request, 'accounts/signup.html', {'error':'Please enter your first and last name!'})
+
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match!'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def settings(request):
     return render(request, 'accounts/settings.html')
--- RoomScout/main/urls.py
+++ RoomScout/main/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from accounts.views import SignUp, login, additionalinfo, settings
+from accounts.views import signup, login, settings
 
 urlpatterns = [
     path('', views.home, name='home'),
 
     # Accounts
-    path('signup', SignUp.as_view() ,name='signup'),
+    path('signup', signup ,name='signup'),
     path('settings', settings ,name='settings'),
-    path('additionalinfo', additionalinfo, name='additionalinfo'),
     path('login', LoginView.as_view() ,name='login'),
     path('logout', LogoutView.as_view() ,name='logout'),
 ]

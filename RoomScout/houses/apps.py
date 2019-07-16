dee263b678e86cc9b6ff26ce18b022013ff55318
Aaron Spindler <nova1104@live.com>
2019-07-15 23:12:27 -0400

    Trimmed migrations

--- RoomScout/houses/apps.py
+++ RoomScout/houses/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class HousesConfig(AppConfig):
     name = 'houses'
--- RoomScout/houses/models.py
+++ RoomScout/houses/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 from accounts.models import User
 from django_countries.fields import CountryField
-from utils.datetime import now
 from accounts.models import User
 
 class House(models.Model):
+    key = models.CharField(primary_key=True, max_length=12)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)
+    is_available = models.BooleanField(default=True)
+
     address = models.CharField(max_length=400)
     country = CountryField()
     prov_state = models.CharField(max_length = 2)
     postal_code = models.CharField(max_length = 6)
-    date_posted = models.DateTimeField(default=now())
+    date_posted = models.DateTimeField()
     kijiji_link = models.URLField(default="")
-    is_approved = models.BooleanField(default=False)
-    is_available = models.BooleanField(default=True)
 
 class Room(models.Model):
+    key = models.CharField(primary_key=True, max_length=12)
     house = models.ForeignKey(House, on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
-    price = models.FloatField(default=0.0)
     tenant = models.ForeignKey(User, on_delete=models.CASCADE,default='')
 
+    price = models.FloatField(default=0.0)
     image = models.ImageField(upload_to='images/', blank=True)
     image2 = models.ImageField(upload_to='images/', blank=True)
     image3 = models.ImageField(upload_to='images/', blank=True)

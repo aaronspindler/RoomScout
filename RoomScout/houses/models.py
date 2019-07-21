75f6349410dbc8b9162ac50670b161fbc39624dc
Aaron Spindler <nova1104@live.com>
2019-07-21 01:30:09 -0400

    Working on models and added an initial database state save file

--- RoomScout/houses/models.py
+++ RoomScout/houses/models.py
@@ -7,13 +7,14 @@ class House(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_approved = models.BooleanField(default=False)
     is_available = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     address = models.CharField(max_length=400)
     country = CountryField(default='CA')
     city = models.CharField(max_length=400, default='')
     prov_state = models.CharField(max_length = 2, default='')
     postal_code = models.CharField(max_length = 7, default='')
-    date_posted = models.DateTimeField()
     kijiji_link = models.URLField(default="")
 
     def full_address(self):
--- RoomScout/rooms/models.py
+++ RoomScout/rooms/models.py
@@ -5,6 +5,8 @@ from houses.models import House
 class Room(models.Model):
     name = models.CharField(max_length=200, default='')
     is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     price = models.FloatField(default=0.0)
     image = models.ImageField(upload_to='images/', blank=True)

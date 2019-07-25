cb42f65ea1cc2ac5b7ed2116b5d6504641e68bde
Aaron Spindler <nova1104@live.com>
2019-07-25 00:53:26 -0400

    Working on make google address search a submittable form

--- RoomScout/houses/models.py
+++ RoomScout/houses/models.py
@@ -5,20 +5,6 @@ from accounts.models import User
 
 
 class House(models.Model):
-	prov_state_choices = (
-		('ON', 'Ontario'),
-		('QC', 'Quebec'),
-		('NS', 'Nova Scotia'),
-		('NB', 'New Brunswick'),
-		('MB', 'Manitoba'),
-		('BC', 'British Columbia'),
-		('PE', 'Prince Edward Island'),
-		('SK', 'Saskatchewan'),
-		('AB', 'Alberta'),
-		('NL', 'Newfoundland & Labrador'),
-		('NA', 'Not Applicable')
-	)
-
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	is_approved = models.BooleanField(default=False)
 	is_available = models.BooleanField(default=False)
@@ -28,7 +14,7 @@ class House(models.Model):
 	address = models.CharField(max_length=400)
 	country = CountryField(default='CA')
 	city = models.CharField(max_length=400, default='')
-	prov_state = models.CharField(max_length=2, choices=prov_state_choices)
+	prov_state = models.CharField(max_length=2)
 	postal_code = models.CharField(max_length=7, default='')
 
 	hide_address = models.BooleanField(default=False)

2d14af00f222aac3a5e178a5168a00a6d09c62bc
Aaron Spindler <nova1104@live.com>
2019-08-05 23:32:16 -0400

    Trying to get heroku to work

--- RoomScout/RoomScout/settings.py
+++ RoomScout/RoomScout/settings.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -188,5 +187,3 @@ else:
 		from .local_settings import *
 	except ImportError:
 		pass
-
-django_heroku.settings(locals())

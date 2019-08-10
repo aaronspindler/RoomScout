4d23572c8a661316c6037a01dfebd2b82df57552
Aaron Spindler <nova1104@live.com>
2019-08-10 15:49:51 -0400

    Still trying to get static working

--- Roomscout/settings.py
+++ Roomscout/settings.py
@@ -163,6 +163,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

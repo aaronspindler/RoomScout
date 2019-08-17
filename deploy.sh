95021f62ef85db6648e3d2094c4cdd53d4a8ecb6
Aaron Spindler <nova1104@live.com>
2019-08-17 18:07:54 -0400

    Working on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 echo "Deploying Roomscout.ca"
 
+cd Roomscout
 echo "Renaming Files"
-mv Roomscout/settings.py Roomscout/settings_dev.py
-mv Roomscout/settings_prod.py Roomscout/settings.py
+mv settings.py settings_dev.py
+mv settings_prod.py settings.py
 
 cd ..
 git add .
@@ -12,6 +13,7 @@ git commit -m "$commitmsg"
 
 git push
 
+cd Roomscout
 echo "Changing filenames back"
-mv Roomscout/settings.py Roomscout/settings_prod.py
-mv Roomscout/settings_dev.py Roomscout/settings.py
+mv settings.py settings_prod.py
+mv settings_dev.py settings.py

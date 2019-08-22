94cd8bb841c2393fbc3c465719fe448c7c01d3cd
Aaron Spindler <nova1104@live.com>
2019-08-22 11:16:04 -0400

    Working on bills model

--- bills/admin.py
+++ bills/admin.py
@@ -1,1 +1,4 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Bill
+
+admin.site.register(Bill)
--- bills/models.py
+++ bills/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+
+from accounts.models import User
+from houses.models import House
+
+class Bill(models.Model):
+	TYPE_CHOICES = [('ElEC', 'Electricity'),('WATER','Water'), ('GAS', 'Gas'), ('INTER','Internet'), ('OTHER', 'Other')]
+
+	type = models.CharField(choices=TYPE_CHOICES, max_length=5)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	house = models.ForeignKey(House, on_delete=models.CASCADE)
+	is_approved = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	date = models.DateField(auto_now=True)
+	file = models.FileField(default=None)
+	amount = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)

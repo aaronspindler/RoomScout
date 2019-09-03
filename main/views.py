26f2c3da599c0adbd33a454d514d1eec5690dd36
Aaron Spindler <nova1104@live.com>
2019-09-03 17:02:17 -0400

    Made custom error pages

--- main/views.py
+++ main/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
 
-
 def home(request):
 	return render(request, 'main/home.html')
-
 
 def about(request):
 	return render(request, 'main/about.html')
 
-
 def contactus(request):
 	return render(request, 'main/contactus.html')
-
 
 def licenses(request):
 	return render(request, 'main/licenses.html')
 
-
 def privacypolicy(request):
 	return render(request, 'main/privacypolicy.html')
 
-
 def termsofuse(request):
 	return render(request, 'main/termsofuse.html')
+
+def permission_denied(request, exception):
+	print(exception.message)
+	return render(request, 'main/403.html')
+
+def page_not_found(request, exception):
+	return render(request, 'main/404.html')
+
+def server_error(request):
+	return render(request, 'main/500.html')

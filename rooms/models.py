57a997048def51f38f8315bd193fb887834fbb32
Aaron Spindler <nova1104@live.com>
2019-09-19 16:24:01 -0400

    More work on iquiry

--- rooms/models.py
+++ rooms/models.py
@@ -26,5 +26,6 @@ class Inquiry(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	room = models.ForeignKey(Room, on_delete=models.CASCADE)
 	message = models.TextField(default='')
 	move_in_date = models.DateField(default='1997-11-04')

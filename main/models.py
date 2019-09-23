d4b7343ba5f8eb1f9ef9391c85f80420007fed40
Aaron Spindler <nova1104@live.com>
2019-09-22 21:22:48 -0400

    Finished contact us

--- main/models.py
+++ main/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.db import models
+
+class ContactMessage(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	sender = models.EmailField(default='')
+	subject = models.TextField(default='')
+	message = models.TextField(default='')
+	ip = models.GenericIPAddressField()
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from utils.captcha import Captcha
 from utils.emailclient import send_contact_us_email
 from utils.ipaddress import get_IP
+from .models import ContactMessage
 
 def home(request):
 	return render(request, 'main/home.html')
@@ -15,6 +17,14 @@ def contactus(request):
 	if request.method == 'POST':
 		ip = get_IP(request)
 		send_contact_us_email(request.POST['sender_email'],request.POST['subject'],request.POST['message'], ip)
+		message = ContactMessage()
+		message.sender = request.POST['sender_email']
+		message.subject = request.POST['subject']
+		message.message = request.POST['message']
+		message.ip = ip
+		message.save()
+
+		messages.success(request, 'We have received your contact request and will get back to you as soon as possible!.')
 		return redirect('home')
 	return render(request, 'main/contactus.html', {'captcha':captcha})
 
@@ -28,7 +38,6 @@ def termsofuse(request):
 	return render(request, 'main/termsofuse.html')
 
 def permission_denied(request, exception):
-	print(exception.message)
 	return render(request, 'main/403.html')
 
 def page_not_found(request, exception):

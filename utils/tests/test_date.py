defdbacefa40a2fdd688150cdd8b3b7bb7bc0673
Aaron Spindler <nova1104@live.com>
2019-10-05 22:52:30 -0400

    Finished date.check_format and tests for it

--- utils/tests/test_date.py
+++ utils/tests/test_date.py
@@ -8,6 +8,7 @@ class DateTest(TestCase):
     def test_check_format(self):
         print('Testing utils.date.check_format()')
         self.assertEqual(check_format('1997-11-04'), True)
+        self.assertEqual(check_format('1234/1234/1234'), False)
         self.assertEqual(check_format('11-04-1997'), False)
         self.assertEqual(check_format('11/04/1997'), False)
         self.assertEqual(check_format('1997/11/04'), False)

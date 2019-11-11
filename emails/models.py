6cadaef7fa42ce5053809c2ede6e0c3d31f223e5
Aaron Spindler <nova1104@live.com>
2019-11-10 20:54:59 -0500

    Copied email functions from utils and created email model

--- emails/models.py
+++ emails/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailMessage(models.Model):
+	sent_at = models.DateTimeField(auto_now_add=True)
+	
+	from_email = models.EmailField()
+	to_email = models.EmailField()
+	subject = models.TextField()
+	text_content = models.TextField()
+	html_content = models.TextField()

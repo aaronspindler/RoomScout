309629c8f90ec53d8992acdeeeae50206029e604
Aaron Spindler <nova1104@live.com>
2019-11-10 21:11:30 -0500

    Moved email templates from utils to emails app

--- emails/senders.py
+++ emails/senders.py
@@ -10,7 +10,7 @@ def send_invite_email(to_email, invite):
 	to_email = to_email
 	subject = "RoomScout | You've been invited to join a house"
 	text_content = 'You have been invited to join a house on RoomScout.ca, sign up or login now to view'
-	html_content = render_to_string('utils/house_invite_template.html', {'invite': invite})
+	html_content = render_to_string('emails/house_invite_template.html', {'invite': invite})
 
 	database_msg = EmailMessage()
 	database_msg.from_email = from_email
@@ -31,7 +31,7 @@ def send_inquiry_email(to_email, inquiry):
 	to_email = to_email
 	subject = "RoomScout | Someone is interested in your room"
 	text_content = 'Someone is interested in ' + inquiry.room.name + ' at ' + inquiry.room.house.full_address()
-	html_content = render_to_string('utils/room_inquiry_template.html', {'inquiry': inquiry})
+	html_content = render_to_string('emails/room_inquiry_template.html', {'inquiry': inquiry})
 
 	database_msg = EmailMessage()
 	database_msg.from_email = from_email
@@ -53,7 +53,7 @@ def send_contact_us_email(submitter_email, submitter_subject, submitter_message,
 	to_email = 'aaron@xnovax.net'
 	subject = "RoomScout | Someone has tried to contact us!"
 	text_content = 'Someone has tried to contact us'
-	html_content = render_to_string('utils/contact_us_template.html', {'email': submitter_email, 'subject': submitter_subject, 'message': submitter_message, 'ip': ip})
+	html_content = render_to_string('emails/contact_us_template.html', {'email': submitter_email, 'subject': submitter_subject, 'message': submitter_message, 'ip': ip})
 
 	database_msg = EmailMessage()
 	database_msg.from_email = from_email
@@ -81,7 +81,7 @@ def send_bill_email(house, bill):
 		# TODO: Add a way to randomize email with funny text
 		text_content = 'Your landlord has added a new bill to your house!'
 		# TODO: Improve template to include more details
-		html_content = render_to_string('utils/bill_added_template.html', {'bill': bill, 'house': house})
+		html_content = render_to_string('emails/bill_added_template.html', {'bill': bill, 'house': house})
 
 		for email in to_emails:
 			database_msg = EmailMessage()

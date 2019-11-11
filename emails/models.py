a51b4f2545b32540aab1866ec206dd3b53709b33
Aaron Spindler <nova1104@live.com>
2019-11-10 21:08:43 -0500

    Added email tracking

--- emails/models.py
+++ emails/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class EmailMessage(models.Model):
+
 	sent_at = models.DateTimeField(auto_now_add=True)
 	
 	from_email = models.EmailField()
--- emails/senders.py
+++ emails/senders.py
@@ -1,6 +1,8 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+from .models import EmailMessage
+
 
 # TODO Put this into a celery background task
 def send_invite_email(to_email, invite):
@@ -9,6 +11,14 @@ def send_invite_email(to_email, invite):
 	subject = "RoomScout | You've been invited to join a house"
 	text_content = 'You have been invited to join a house on RoomScout.ca, sign up or login now to view'
 	html_content = render_to_string('utils/house_invite_template.html', {'invite': invite})
+
+	database_msg = EmailMessage()
+	database_msg.from_email = from_email
+	database_msg.to_email = to_email
+	database_msg.subject = subject
+	database_msg.text_content = text_content
+	database_msg.html_content = html_content
+	database_msg.save()
 
 	msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
 	msg.attach_alternative(html_content, "text/html")
@@ -23,6 +33,14 @@ def send_inquiry_email(to_email, inquiry):
 	text_content = 'Someone is interested in ' + inquiry.room.name + ' at ' + inquiry.room.house.full_address()
 	html_content = render_to_string('utils/room_inquiry_template.html', {'inquiry': inquiry})
 
+	database_msg = EmailMessage()
+	database_msg.from_email = from_email
+	database_msg.to_email = to_email
+	database_msg.subject = subject
+	database_msg.text_content = text_content
+	database_msg.html_content = html_content
+	database_msg.save()
+
 	msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
 	msg.attach_alternative(html_content, "text/html")
 	msg.send()
@@ -36,6 +54,14 @@ def send_contact_us_email(submitter_email, submitter_subject, submitter_message,
 	subject = "RoomScout | Someone has tried to contact us!"
 	text_content = 'Someone has tried to contact us'
 	html_content = render_to_string('utils/contact_us_template.html', {'email': submitter_email, 'subject': submitter_subject, 'message': submitter_message, 'ip': ip})
+
+	database_msg = EmailMessage()
+	database_msg.from_email = from_email
+	database_msg.to_email = to_email
+	database_msg.subject = subject
+	database_msg.text_content = text_content
+	database_msg.html_content = html_content
+	database_msg.save()
 
 	msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
 	msg.attach_alternative(html_content, "text/html")
@@ -56,6 +82,15 @@ def send_bill_email(house, bill):
 		text_content = 'Your landlord has added a new bill to your house!'
 		# TODO: Improve template to include more details
 		html_content = render_to_string('utils/bill_added_template.html', {'bill': bill, 'house': house})
+
+		for email in to_emails:
+			database_msg = EmailMessage()
+			database_msg.from_email = from_email
+			database_msg.to_email = email
+			database_msg.subject = subject
+			database_msg.text_content = text_content
+			database_msg.html_content = html_content
+			database_msg.save()
 
 		msg = EmailMultiAlternatives(subject, text_content, from_email, to_emails)
 		msg.attach_alternative(html_content, "text/html")

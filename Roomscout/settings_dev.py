2d2b21434899fa2e11dab33c45f8d92cf8549b6a
Aaron <aaron@xnovax.net>
2019-11-21 14:14:00 -0500

    Added stripe keys

--- Roomscout/settings_dev.py
+++ Roomscout/settings_dev.py
@@ -7,6 +7,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
+# Stripe
+STRIPE_KEY = ''
+STRIPE_SECRET_KEY = ''
+
 # Google
 GOOGLE_API_KEY = ''
 RECAPTCHA_PUBLIC_KEY = ''

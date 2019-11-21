7d46530cb0cc9baf7229a6890767aea5bc9e8eb7
Aaron <aaron@xnovax.net>
2019-11-21 17:24:05 -0500

    Fixed an indentation

--- utils/streetview.py
+++ utils/streetview.py
@@ -3,6 +3,7 @@ from django.core.files import File
 from .models import HouseImage
 import urllib.request
 
+
 def load_house_image(house):
 	try:
 		url = 'https://maps.googleapis.com/maps/api/streetview?size=640x480&location={},{}&key={}'.format(house.lat, house.lon, settings.GOOGLE_API_KEY)

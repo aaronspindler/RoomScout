dc76144382b1c34592e82557fb3e976bb78eafd1
Aaron <aaron@xnovax.net>
2019-11-22 13:57:58 -0500

    Prod settings

--- Roomscout/settings.py
+++ Roomscout/settings.py
@@ -1,29 +1,30 @@
 import os
-import sys
+
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Stripe
-STRIPE_KEY = ''
-STRIPE_SECRET_KEY = ''
+STRIPE_KEY = os.environ['STRIPE_KEY']
+STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
 
 # Google
-GOOGLE_API_KEY = ''
-RECAPTCHA_PUBLIC_KEY = ''
-RECAPTCHA_PRIVATE_KEY = ''
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+RECAPTCHA_PUBLIC_KEY = os.environ['RECAPTCHA_PUBLIC_KEY']
+RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
 RECAPTCHA_USE_SSL = True
 ACCOUNT_SIGNUP_FORM_CLASS = 'accounts.forms.AllauthSignupForm'
 
 # Walk Score
-WALK_SCORE_API = ''
+WALK_SCORE_API = os.environ['WALK_SCORE_API']
 
 # AWS
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_DEFAULT_ACL = None
 
 AWS_PUBLIC_MEDIA_LOCATION = ''
@@ -39,7 +40,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['roomscout.ca', 'www.roomscout.ca', 'roomscout.herokuapp.com', 'roomscout-dev.herokuapp.com']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -64,9 +65,9 @@ ACCOUNT_LOGOUT_REDIRECT = 'home'
 # Email
 DEFAULT_FROM_EMAIL = 'services@roomscout.ca'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -113,6 +114,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Roomscout.urls'
@@ -139,7 +141,7 @@ WSGI_APPLICATION = 'Roomscout.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-	'default': ''
+	'default': dj_database_url.config()
 }
 
 # Password validation
@@ -177,23 +179,23 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATICFILES_DIRS = []
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Commented out because it makes Heroku deployment error when Debug=False
+# TODO: Figure out why this happens
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
-try:
-	from .local_settings import *
-except ImportError:
-	pass
-
-if 'test' in sys.argv:
-	DATABASES['default'] = {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': 'testingdb'
-	}
+# Security
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 314536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True

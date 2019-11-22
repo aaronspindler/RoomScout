0747311d4c036c6163576ab160066f0090d41627
Aaron <aaron@xnovax.net>
2019-11-22 13:57:18 -0500

    Added tests for main and eggs

--- eggs/tests.py
+++ eggs/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from .views import get_dog_image
 
-# Create your tests here.
+
+class EggsTests(TestCase):
+    def test_dog_view(self):
+        print("Testing eggs.views.dog()")
+        response = self.client.get(reverse('ee_dog'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Thank you for your kind donation')
+        self.assertNotContains(response, 'This should not be contained!')
+        self.assertTemplateUsed(response, 'eggs/dog.html')
+
+    def test_get_dog_image(self):
+        print("Testing eggs.views.get_dog_image()")
+        image1 = get_dog_image()
+        self.assertIsNotNone(image1)
+        image2 = get_dog_image()
+        self.assertNotEqual(image1, image2)
+

23858a1449d62050278d547496441f017986dda4
Aaron Spindler <nova1104@live.com>
2019-12-01 17:54:33 -0500

    Finished house model tests

---
+++ houses/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class HousesViewsTests(TestCase):
+	def setUp(self):
+		self.client = Client()
+		User = get_user_model()
+		self.user = User.objects.create_user(username='FredFlintstone', email='fred@flintstone.com', password='babadoo')
+
+	def test_house_create_page(self):
+		print('Testing house creation page')
+		self.client.login(username='Fred_Flintstone', password='foo')
+		response = self.client.get(reverse('house_create'))
+		self.assertEqual(response.status_code, 200)

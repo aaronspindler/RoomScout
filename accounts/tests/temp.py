47d90b3b982e8a4626566adb779788bedec28e36
Aaron Spindler <nova1104@live.com>
2019-12-01 16:04:43 -0500

    Removed temp.py

--- accounts/tests/temp.py
+++
@@ -1,6 +0,0 @@
-def test_email_unsubscribe_view_authenticated(self):
-	print('Testing accounts.views.unsubscribe() authenticated')
-
-
-def test_email_unsubscribe_view_unauthenticated(self):
-	print('Testing accounts.views.unsubscribe() unauthenticated')

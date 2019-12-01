cb58e823a9c9f6b88e85dbc713b4679cc4654707
Aaron Spindler <nova1104@live.com>
2019-12-01 16:08:08 -0500

    Added test directory for bills

---
+++ bills/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+
+class BillsModelTests(TestCase):
+	def setUp(self):
+		self.client = Client()
+		User = get_user_model()
+		self.user = User.objects.create_user(username='FredFlintstone', email='fred@flintstone.com', password='babadoo')

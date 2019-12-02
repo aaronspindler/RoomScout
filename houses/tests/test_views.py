35ae775df55586087a777efc25c15c81f6f3341f
Aaron Spindler <nova1104@live.com>
2019-12-01 23:04:47 -0500

    Working on houses view tests

--- houses/tests/test_views.py
+++ houses/tests/test_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
+
 class HousesViewsTests(TestCase):
 	def setUp(self):
 		self.client = Client()

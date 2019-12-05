5b7c8b62f9744777d69f8ddac2482bff03103036
Aaron Spindler <nova1104@live.com>
2019-12-05 14:14:31 -0500

    Starting on bill tests

--- bills/views.py
+++ bills/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, get_object_or_404
 
 from .models import Bill, BillSet
 
+
 # PK is the primary key for the bill
 def bill_delete(request, pk):
 	delete_set = False
@@ -11,13 +12,13 @@ def bill_delete(request, pk):
 	house = bill.set.house
 	if request.user != bill.user:
 		raise Http404
-
+	
 	if Bill.objects.filter(set=bill.set).count() == 1:
 		delete_set = True
-
+	
 	bill.delete()
-
+	
 	if delete_set == True:
 		BillSet.objects.filter(pk=set_key).delete()
-
+	
 	return redirect('house_detail', house.pk)

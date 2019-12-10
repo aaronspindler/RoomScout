43690ecb1215e604f9a188f7a00dba9b7a6900b9
Aaron Spindler <nova1104@live.com>
2019-12-10 13:56:29 -0500

    backed to prod database

--- Roomscout/settings_prod.py
+++ Roomscout/settings_prod.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 import dj_database_url
 
@@ -142,12 +141,8 @@ WSGI_APPLICATION = 'Roomscout.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
 DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': 'testingdb'
-		}
+	'default': dj_database_url.config()
 }
 
 # Password validation

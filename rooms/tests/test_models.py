5bfa30f65d04207cec2ba3fce47378115131ed13
Aaron <aaron@xnovax.net>
2019-12-18 11:24:56 -0500

    Started tests for rooms

--- rooms/tests/test_models.py
+++ rooms/tests/test_models.py
@@ -1,8 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+from houses.models import House
 
 
 class RoomsModelTests(TestCase):
-	pass
+	def setUp(self):
+		self.client = Client()
+		User = get_user_model()
+		self.user = User.objects.create_user(username='FredFlintstone', email='aaron@xnovax.net', password='babadoo')
+		self.user2 = User.objects.create_user(username='JackyFlintstone', email='jacky@flintstone.com', password='lovefred')
+		house = House.objects.create(user=self.user)
+		house.place_id = 'EiwyNTI5IFN0YWxsaW9uIERyLCBPc2hhd2EsIE9OIEwxSCA3SzQsIENhbmFkYSIxEi8KFAoSCY_JD3vDG9WJEe3JFhlBvwOKEOETKhQKEgnrS9FlwxvViRHYx20MM9m-8g'
+		house.lat = '43.95858010000001'
+		house.lon = '-78.91587470000002'
+		house.street_number = 2529
+		house.street_name = 'Stallion Drive'
+		house.city = 'Oshawa'
+		house.prov_state = 'ON'
+		house.postal_code = 'L1H 0M4'
+		house.country = 'Canada'
+
+		house.save()
+		self.house = house
+
+	def test_room_creation(self):
+		pass
 
 
 class InquiryModelTests(TestCase):

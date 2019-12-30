0b3a950e67f1e2c0cfb68a72cb248cc858b3c758
Aaron <aaron@xnovax.net>
2019-12-30 15:27:44 -0500

    Added robots.txt

--- Roomscout/urls.py
+++ Roomscout/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from blog.sitemaps import BlogSitemap
 from houses.sitemaps import HouseSitemap
@@ -19,6 +20,7 @@ sitemaps = {
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+	path('robots.txt', TemplateView.as_view(template_name="robots/robots.txt", content_type='text/plain')),
 	path('', include('main.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

16ea9f1deaf138e5a60ab472e1805bc388b70e12
Aaron <aaron@xnovax.net>
2020-01-02 13:24:49 -0500

    Still working on bill form

--- bills/forms.py
+++ bills/forms.py
@@ -4,11 +4,8 @@ from django import forms
 from bills.models import Bill
 
 
-class CreateBillForm(ModelForm):
+class CreateBillForm(forms.Form):
+    date = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
+    type = forms.ChoiceField(choices=Bill.TYPE_CHOICES)
+    amount = forms.DecimalField()
     file = forms.FileField()
-    class Meta:
-        model = Bill
-        fields = ['date', 'type', 'amount']
-        widgets = {
-            'date': forms.DateInput(attrs={'class': 'datepicker', 'id': 'date'})
-        }

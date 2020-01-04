fa6bc642a12ce49214c96c0666a3165f7ec3df00
Aaron Spindler <nova1104@live.com>
2020-01-04 14:26:07 -0500

    Still trying to get date working

--- bills/forms.py
+++ bills/forms.py
@@ -4,8 +4,10 @@ from django import forms
 from bills.models import Bill
 
 
-class CreateBillForm(forms.Form):
-    date = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
-    type = forms.ChoiceField(choices=Bill.TYPE_CHOICES)
-    amount = forms.DecimalField()
-    file = forms.FileField()
+class CreateBillForm(forms.ModelForm):
+    class Meta:
+        model = Bill
+        fields = ['date', 'type', 'amount']
+        widgets = {
+            'date': forms.DateInput()
+        }

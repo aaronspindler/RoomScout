3f81f84fba228f5d71461f1def5602e75377117a
Aaron Spindler <nova1104@live.com>
2020-01-06 15:51:46 -0500

    Added basic tests for new bill form

--- bills/tests/test_forms.py
+++ bills/tests/test_forms.py
@@ -1,24 +1,45 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase, Client
-from houses.models import House
+from django.test import TestCase
+
+from bills.forms import BillForm, BillFormset
 
 
 class BillFormTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-        User = get_user_model()
-        self.user = User.objects.create_user(username='FredFlintstone', email='aaron@xnovax.net', password='babadoo')
-        self.user2 = User.objects.create_user(username='JackyFlintstone', email='jacky@flintstone.com', password='lovefred')
-        house = House.objects.create(user=self.user)
-        house.place_id = 'EiwyNTI5IFN0YWxsaW9uIERyLCBPc2hhd2EsIE9OIEwxSCA3SzQsIENhbmFkYSIxEi8KFAoSCY_JD3vDG9WJEe3JFhlBvwOKEOETKhQKEgnrS9FlwxvViRHYx20MM9m-8g'
-        house.lat = '43.95858010000001'
-        house.lon = '-78.91587470000002'
-        house.street_number = 2529
-        house.street_name = 'Stallion Drive'
-        house.city = 'Oshawa'
-        house.prov_state = 'ON'
-        house.postal_code = 'L1H 0M4'
-        house.country = 'Canada'
+    def test_BillForm_creation_valid(self):
+        print('Testing bills.forms.BillForm() creation valid')
+        form = BillForm({'date': '2019-11-04', 'type': 'ELEC', 'amount': 193.33})
+        self.assertTrue(form.is_valid())
 
-        house.save()
-        self.house = house
+    def test_BillForm_creation_invalid(self):
+        print('Testing bills.forms.BillForm() creation invalid')
+        form = BillForm({'date': '', 'type': '', 'amount': -100})
+        self.assertFalse(form.is_valid())
+
+
+class BillFormsetTests(TestCase):
+    def test_BillFormset_creation_valid(self):
+        print('Testing bills.forms.BillFormset() creation valid')
+        formset = BillFormset(
+            {
+             'form-INITIAL_FORMS': '0',
+             'form-TOTAL_FORMS': '1',
+             'form-0-date': '2019-11-04',
+             'form-0-type': 'ELEC',
+             'form-0-amount': 299.99,
+             'form-0-file': None
+            }
+        )
+        self.assertTrue(formset.is_valid())
+
+    def test_BillFormset_creation_invalid(self):
+        print('Testing bills.forms.BillFormset() creation invalid')
+        formset = BillFormset(
+            {
+             'form-INITIAL_FORMS': '0',
+             'form-TOTAL_FORMS': '2',
+             'form-0-date': '',
+             'form-0-type': '',
+             'form-0-amount': -100,
+             'form-0-file': None
+            }
+        )
+        self.assertFalse(formset.is_valid())

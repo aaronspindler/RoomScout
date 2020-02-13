659f951b68e93fcc43b93549a8119d5024058a9e
Aaron Spindler <nova1104@live.com>
2020-02-13 14:40:46 -0500

    Added framework for begining tests for security app

---
+++ security/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+
+class SecurityModelTests(TestCase):
+	def setUp(self):
+		self.client = Client()
+		User = get_user_model()
+		self.user = User.objects.create_user(username='FredFlintstone', email='aaron@xnovax.net', password='babadoo')
+		self.user2 = User.objects.create_user(username='JackyFlintstone', email='jacky@flintstone.com', password='lovefred')
---
+++ security/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class SecurityViewTests(TestCase):
+	def setUp(self):
+		self.client = Client()
+		User = get_user_model()
+		self.user = User.objects.create_user(username='FredFlintstone', email='aaron@xnovax.net', password='babadoo')
+		self.user2 = User.objects.create_user(username='JackyFlintstone', email='jacky@flintstone.com', password='lovefred')
